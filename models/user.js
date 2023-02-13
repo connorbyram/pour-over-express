dbac1974f335c9b1afc2d63b11a400e7495926bc
connorbyram <connorabyram@gmail.com>
2023-02-12 23:05:56 -0800

    Google OAuth implemented

---
+++ models/user.js
@@ -0,0 +1,16 @@
+mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: String,
+  googleId: {
+    type: String,
+    required: true
+  },
+  email: String,
+  avatar: String
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema);
